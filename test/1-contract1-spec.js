0da714404ca1eb15488131700511ea191a3400ae
James Bachini <james@mcambridge.com>
2022-05-14 09:15:42 +0100

    add kovan contract addresses

--- test/1-contract1-spec.js
+++ test/1-contract1-spec.js
@@ -1,6 +1,11 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+// Kovan
+const lido = 0x4b7FCBC11BB45075b9A1F953128C09bC97D6a0D7; // https://docs.lido.fi/deployed-contracts
+const aave = 0x9D2729bC36f9E203002Bc5B5ee2E08C68Bd13794; // https://docs.aave.com/developers/deployed-contracts/v3-testnet-addresses
+const dydx = 0x4EC3570cADaAEE08Ae384779B0f3A45EF85289DE; // https://www.reddit.com/r/dydxprotocol/comments/msv2it/dydx_kovan_test_network_address/
+
 describe("Greeter", function () {
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
